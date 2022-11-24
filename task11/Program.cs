68ccda1a6d3dacab9f3a4075d7bb4f72faceb383
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2022-11-24 16:34:28 +1000

    add task11

---
+++ task11/Program.cs
@@ -0,0 +1,35 @@
+﻿// Проверка на ввод только цифр через Console.ReadLine().
+
+string InputOnlyNumbers()
+{
+    char[] numbers = { '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    bool success = false;
+    string input = string.Empty;
+    while (success != true)
+    {
+        Console.Write("Введите число: ");
+        input = Console.ReadLine();
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = 0; j < numbers.Length; j++)
+            {
+                if (input[i] == numbers[j])
+                {
+                    success = true;
+                    if (input[i] == '-' && i != 0) success = false;
+                    break;
+                }
+                else success = false;
+            }
+            if (success == false)
+            {
+                Console.WriteLine("Введены неверные данные");
+                break;
+            }
+        }
+    }
+    return input;
+}
+
+int number = Convert.ToInt32(InputOnlyNumbers());
+Console.WriteLine($"Число: {number}");
