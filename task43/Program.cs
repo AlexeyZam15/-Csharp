4fe13d9d7c8b5c4d7fe69e4b23ccc8f90235f2b1
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-09 21:00:50 +1000

    add task43

---
+++ task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Даны два натуральных числа a и b.  
+// Вычислите НОД(a,b) - наибольший общий делитель а и b.
+
+int a = 0;
+while (a <= 0)
+{
+    Console.WriteLine("Введите натуральное число a:");
+    a = Convert.ToInt32(Console.ReadLine());
+    if (a <= 0)
+        Console.WriteLine("Введены неверные данные");
+}
+
+int b = 0;
+while (b <= 0)
+{
+    Console.WriteLine("Введите натуральное число b:");
+    b = Convert.ToInt32(Console.ReadLine());
+    if (b <= 0)
+        Console.WriteLine("Введены неверные данные");
+}
+
+int nod = 1;
+int max = a;
+if (b > max) max = b;
+for (int i = max; i > 1; i--)
+{
+    if (a % i == 0 & b % i == 0)
+    {
+        nod = i;
+        break;
+    }
+}
+
+Console.WriteLine($"НОД({a},{b}) = {nod}");
