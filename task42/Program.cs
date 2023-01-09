16e7de1816693942b0165ed48cad8f0ab8212f0d
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-09 20:33:21 +1000

    add task42

---
+++ task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дано  натуральное  n,  вычислите 1/0! + 1/1!+ ... + 1/n!
+
+int n = 0;
+while (n <= 0)
+{
+    Console.WriteLine("Введите натуральное число n:");
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n <= 0)
+        Console.WriteLine("Введены неверные данные");
+}
+
+double sum = 1;
+double factorial = 1;
+for (double i = 1; i <= n; i++)
+{
+    factorial *= i;
+    sum += 1 / factorial;
+}
+
+Console.WriteLine($"1/0! + 1/1!+ ... + 1/{n}! = {sum}");
