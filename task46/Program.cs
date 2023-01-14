bb3ca92f6bb34af0586f0b3a54980fe7c90cea51
Alexey Zamaraev <www.wolfy.ky@mail.ru>
2023-01-14 19:24:52 +1000

    Modified task46

--- task46/Program.cs
+++ task46/Program.cs
@@ -4,30 +4,35 @@ Random rnd = new Random();
 int hiddenNumber = rnd.Next(1, 101);
 Console.WriteLine($"Загаданное число - {hiddenNumber}");
 
-int IntBinarySearchWithPrintAlgorithm(int number, int min, int max)
+int IntBinarySearchWithPrintAlgorithm(int number, int min, int max, bool log = false)
 {
     int foundNumber = max;
     while (true)
     {
+        if (log == true)
         Console.Write($"{min,4} {foundNumber,4} {max,4}");
         if (min == number)
         {
+            if (log == true)
             Console.WriteLine($"{"=",3}");
             return min;
         }
         if (number == foundNumber)
         {
+            if (log == true)
             Console.WriteLine($"{"=",3}");
             return foundNumber;
         }
         if (number < foundNumber)
         {
+            if (log == true)
             Console.WriteLine($"{"<",3}");
             max = foundNumber;
             foundNumber -= (foundNumber - min) / 2;
         }
         else
         {
+            if (log == true)
              Console.WriteLine($"{">",3}");
             min = foundNumber;
             foundNumber += (max - min) / 2;
@@ -35,5 +40,5 @@ int IntBinarySearchWithPrintAlgorithm(int number, int min, int max)
     }
 }
 
-int foundNumber = IntBinarySearchWithPrintAlgorithm(hiddenNumber, 1, 100);
+int foundNumber = IntBinarySearchWithPrintAlgorithm(hiddenNumber, 1, 100, true);
 Console.WriteLine($"Найденное число - {foundNumber}");
